5ab11ea598c9fe394915d3a39a0445810f684d62
sorana5 <sorana.gl.maria@gmail.com>
2023-07-08 00:08:16 +0300

    added new songs

---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PortalSpreLumeaMuzicii
+{
+    public partial class Start : Form
+    {
+        public Start()
+        {
+            InitializeComponent();
+        }
+
+        private Dictionary<string, int> convertireNote = new Dictionary<string, int>()
+        {
+            ["do"] = 150,
+            ["re"] = 140,
+            ["mi"] = 130,
+            ["fa"] = 120,
+            ["sol"] = 110,
+            ["la"] = 100,
+            ["si"] = 90,
+            ["do2"] = 80,
+            ["re2"] = 70,
+            ["mi2"] = 60,
+            ["fa2"] = 50,
+            ["sol2"] = 40,
+            ["la2"] = 30,
+            ["si2"] = 20,
+            ["do3"] = 10
+        };
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(@"Cantece.txt");
+            while (!sr.EndOfStream)
+            {
+                string nume = sr.ReadLine();
+                //titlu.Text = nume;
+                string valori = sr.ReadLine();
+                string[] bits = valori.Split(' ');
+                //int nota = sr.Read();
+                List <int> note = new List <int>();
+                foreach (var bit in bits)
+                    note.Add(convertireNote[bit]);
+                Form6.cantece.Add(new Cantec(nume, note));
+                //foreach (var notes in note)
+                    //titlu.Text = titlu.Text + Convert.ToString(notes);
+            }
+            sr.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Cuprins cuprins = new Cuprins();
+            cuprins.ShowDialog();
+            cuprins = null;
+            this.Show();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Form4 frm = new Form4();
+            frm.Show(); 
+        }
+
+        private void titlu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PortalSpreLumeaMuzicii
+{
+    public partial class Cuprins : Form
+    {
+        public Cuprins()
+        {
+            InitializeComponent();
+        }
+
+        private void Cuprins_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form5 form5 = new Form5();
+            form5.ShowDialog();
+            form5 = null;
+            this.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            //Start start = new Start();
+            //start.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 form3 = new Form3();
+            form3.ShowDialog();
+            form3 = null;
+            this.Show();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form4 form4 = new Form4();
+            form4.ShowDialog();
+            form4 = null;
+            this.Show();
+        }
+    }
+}
